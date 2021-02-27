9f75d4313b14ec2d917ad4289a705c192468ec87
kevindavis-gd <kevindavis.gd@gmail.com>
2021-02-27 16:36:03 -0600

    comments

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# Test class to endure that the api works
+# Inherits from the default model class and sets the requirements for the database columns
 class CheckIn(models.Model):
     mustangsID = models.CharField(max_length=100)
     buildingID = models.CharField(max_length=100)
