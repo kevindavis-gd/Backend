af82556ab2d36aee9de39934f7c34bb3d245bea6
kevindavis-gd <kevindavis.gd@gmail.com>
2021-03-19 23:59:11 -0500

    users are able to register

--- users/serializer.py
+++ users/serializer.py
@@ -2,22 +2,31 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.models import User
 
+#User model already exists within Django, we just created the serializer
 class UserSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(
+            max_length=100
+            )
+    last_name = serializers.CharField(
+            max_length=100
+            )
     email = serializers.EmailField(
             required=True,
             validators=[UniqueValidator(queryset=User.objects.all())]
             )
     username = serializers.CharField(
+            required=True,
             max_length=32,
             validators=[UniqueValidator(queryset=User.objects.all())]
             )
-    password = serializers.CharField(min_length=8, write_only=True)
+    password1 = serializers.CharField(min_length=8, write_only=True)
+    password2 = serializers.CharField(min_length=8, write_only=True)
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'],
-             validated_data['password'])
+        user = User.objects.create_user(validated_data['username'],email = validated_data['email'],password=validated_data['password1'],first_name = validated_data['first_name'],last_name = validated_data['last_name'])
+        
         return user
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'first_name','last_name','username', 'email', 'password1','password2')
--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/users/', views.UserCreate.as_view(), name='account-create'),
+    path('createUser/', views.UserCreate.as_view(), name='account-create'),
 ]
