7e011d70585aec3835ada17f323e12a49542e8fd
kevindavis-gd <kevindavis.gd@gmail.com>
2021-03-22 22:19:48 -0500

    token response

--- Backend/settings.py
+++ Backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'api',
     'users',
     'rest_framework',
+    'rest_framework.authtoken',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -128,3 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+  'DEFAULT_AUTHENTICATION_CLASSES': (
+      'rest_framework.authentication.TokenAuthentication',
+    )
+}
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path, include
+# used to obtain token
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('createUser/', views.UserCreate.as_view(), name='account-create'),
+    path('login/', obtain_auth_token, name='obtain-token'),
 ]
